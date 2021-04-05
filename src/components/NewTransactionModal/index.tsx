79b67ca961bf2cf21d1c899dcb8967d3327bb418
kaique caires <kaique.caires@yahoo.com>
2021-04-05 11:11:59 -0300

    feat: create styles of inputs

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,6 @@
 import Modal from 'react-modal';
+import closeImg from '../../assets/close.svg'
+import { Container } from './styles';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -10,8 +12,19 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
     <Modal
       isOpen={isOpen}
       onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
     >
-      <h2>Cadastrar transação</h2>
+      <button type="button" className="react-modal-close" >
+        <img src={closeImg} alt="Fechar Modal" onClick={onRequestClose} />
+      </button>
+      <Container>
+        <h2>Cadastrar transação</h2>
+        <input type="text" placeholder="Titulo" />
+        <input type="number" placeholder="Valor" />
+        <input type="text" placeholder="Categoria" />
+        <button type="submit">Cadastrar</button>
+      </Container>
     </Modal>
   );
 }
