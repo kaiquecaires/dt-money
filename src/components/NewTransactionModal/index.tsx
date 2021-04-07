b09ca205c3252aa0c669fba51876b1c764decbe0
kaique caires <kaique.caires@yahoo.com>
2021-04-07 18:51:04 -0300

    feat: save form values

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
 import Modal from 'react-modal';
 import closeImg from '../../assets/close.svg'
 import incomeImg from '../../assets/income.svg'
@@ -12,6 +12,14 @@ interface NewTransactionModalProps {
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
   const [type, setType] = useState('deposit');
+  const [value, setValue] = useState(0);
+  const [title, setTitle] = useState('');
+  const [category, setCategory] = useState('');
+
+  function handleCreateNewTransaction(event: FormEvent) {
+    event.preventDefault();
+    console.log(title, value, category, type);
+  }
 
   return (
     <Modal
@@ -23,10 +31,10 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
       <button type="button" className="react-modal-close" >
         <img src={closeImg} alt="Fechar Modal" onClick={onRequestClose} />
       </button>
-      <Container>
+      <Container onSubmit={handleCreateNewTransaction}>
         <h2>Cadastrar transação</h2>
-        <input type="text" placeholder="Titulo" />
-        <input type="number" placeholder="Valor" />
+        <input type="text" placeholder="Titulo" value={title} onChange={(e) => { setTitle(e.target.value) }} />
+        <input type="number" placeholder="Valor" value={value} onChange={(e) => { setValue(Number(e.target.value)) }} />
         <TransactionTypeContainer>
           <RadioBox
             type="button"
@@ -47,7 +55,7 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
             <span>Saída</span>
           </RadioBox>
         </TransactionTypeContainer>
-        <input type="text" placeholder="Categoria" />
+        <input type="text" placeholder="Categoria"  value={category} onChange={(e) => { setCategory(e.target.value) }} />
         <button type="submit">Cadastrar</button>
       </Container>
     </Modal>
