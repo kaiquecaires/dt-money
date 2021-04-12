deb21514f4a78ec260454726db9440dd085954fc
kaique caires <kaique.caires@yahoo.com>
2021-04-12 18:14:56 -0300

    feat: create TransactionsProvider

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 import { NewTransactionModal } from './components/NewTransactionModal'
+import { TransactionsProvider } from './TransactionsContext';
 
 Modal.setAppElement('#root');
 
@@ -19,7 +20,7 @@ export function App() {
   }
 
   return (
-    <div className="App">
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
       <NewTransactionModal
@@ -27,6 +28,6 @@ export function App() {
         onRequestClose={handleClosedNewTransactionModal}
       />
       <GlobalStyle />
-    </div>
+    </TransactionsProvider>
   );
 }
